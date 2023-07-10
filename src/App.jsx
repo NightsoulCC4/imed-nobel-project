018027bc9ffb4bcfe0a897321693a8270898e678
Nattatat Tantakan <nattatat@pkindev.com>
2023-07-10 20:22:24 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(0);
+
+  const getData = async () => {
+    await axios
+      .get("https://api.nobelprize.org/2.1/nobelPrizes")
+      .then((res, err) => {
+        if (err) console.log(err);
+        else setData(res);
+      });
+  };
+
+  useEffect(() => {
+    if (data === 0) getData();
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
